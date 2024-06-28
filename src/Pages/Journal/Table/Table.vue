3761fbc0722d2160635b7cd8d07eba613bb37979
VladimirGromyko <vl-mailbox@mail.ru>
2024-06-28 10:17:04 +0300

    Table scroll created

--- src/Pages/Journal/Table/Table.vue
+++ src/Pages/Journal/Table/Table.vue
@@ -10,28 +10,44 @@ const props = defineProps<Props>()
 </script>
 
 <template>
-  <div v-for='(user, index) in props.tableData'
-       :key='`${index}-user`'
-       class="user-table"
-  >
-    <div class="user-column">
-      <img :src='user.picture' alt='' />
+  <div class="table-wrapper">
+    <div v-for='(user, index) in props.tableData'
+         :key='`${index}-user`'
+         class="items"
+    >
+      <div class="table-row">
+        <div class="user-column">
+          <img :src='user.picture' alt='' />
+        </div>
+        <div class="user-column">{{ user.fullName }}</div>
+        <div class="user-column">{{ user.gender }}</div>
+        <div class="user-column">{{ user.country }}</div>
+        <div class="user-column">{{ user.dob }}</div>
+        <div class="user-column e-mail">{{ user.email }}</div>
+        <div class="user-column">{{ user.phone }}</div>
+      </div>
+
     </div>
-    <div class="user-column">{{ user.fullName }}</div>
-    <div class="user-column">{{ user.gender }}</div>
-    <div class="user-column">{{ user.country }}</div>
-    <div class="user-column">{{ user.dob }}</div>
-    <div class="user-column e-mail">{{ user.email }}</div>
-    <div class="user-column">{{ user.phone }}</div>
   </div>
+
 </template>
 
 
 <style scoped lang="scss">
-.user-table {
+.table-wrapper {
+  max-height: 75vh;
+  overflow-y: scroll;
+  padding: 5px 0;
+}
+.items{
+  :hover{
+    background: $neutral150;
+  }
+}
+.table-row {
   display: grid;
   grid-template-columns: 2fr 3fr 2fr 2fr 3fr 5fr auto;
-  padding: 5px 60px 5px 20px;
+  padding: 5px 60px 0 20px;
 }
 .user-column {
   display: flex;
